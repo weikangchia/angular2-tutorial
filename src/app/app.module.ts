df266aedf0d7e09b65fcba628c2cab5283fd53b9
Wei Kang <weikangchia@gmail.com>
2017-01-07 17:43:58 +0800

    Add new page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {CoursesComponent} from "./courses.component";
 import {AuthorsComponent} from "./authors.component";
 import {AutoGrowDirective} from "./auto-grow.directive";
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from './footer/footer.component';
+import {AboutComponent} from './about/about.component';
+import {routes} from "./app.router";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-      AppComponent,
-      CoursesComponent,
-      AuthorsComponent,
-      AutoGrowDirective
+    AppComponent,
+    CoursesComponent,
+    AuthorsComponent,
+    HeaderComponent,
+    AutoGrowDirective,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
